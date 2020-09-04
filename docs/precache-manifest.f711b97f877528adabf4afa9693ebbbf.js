03c075256b80253a4aaad481d880a7163760530d
Luis <lchaconw9260@universidadean.edu.co>
2020-09-04 00:43:54 -0500

    docs agregado

---
+++ docs/precache-manifest.f711b97f877528adabf4afa9693ebbbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ced5aad50a8de9060621f38648f989",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549fbb7c5bb17903a918",
+    "url": "/static/css/main.dc624969.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549fbb7c5bb17903a918",
+    "url": "/static/js/main.bb17cd4d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
